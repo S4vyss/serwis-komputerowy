55014e4de86b0628dabf18c085dbd7de0e3a1b59
S4vyss <92021800+S4vyss@users.noreply.github.com>
2023-05-18 18:12:36 +0200

    half of contact us done

--- routes/contact.php
+++ routes/contact.php
@@ -1,1 +1,64 @@
-<h1>siema contact</h1>
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../styles/global.css" />
+    <link rel="stylesheet" href="../styles/contact.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body class="contact">
+    <?php include '../header.php' ?>
+    <h1>Contact Us</h1>
+    <main class="container-contact">
+        <article class="contact-info">
+            <div class="left-contact">
+                <h2>Contact Information</h2>
+                <p>Fill up the form and our Team will get back to you within 24 hours</p>
+                <div class="icons">
+                    <div>
+                        <div class="icon">
+                            <img width="16" src="../assets/phone.svg" alt="telefon" />
+                            <p>(+48) 323 212 432</p>
+                        </div>
+                        <div class="icon" style="margin-top: 2em">
+                            <img width="16" src="../assets/mail.svg" alt="mail" />
+                            <p>contact@gmail.com</p>
+                        </div>
+                        <div class="icon" style="margin-top: 2em">
+                            <img width="16" src="../assets/location.svg" alt="mail" />
+                            <p>Burger Av. 101 ST.</p>
+                        </div>
+                    </div>
+                    <div class="links">
+                        <a href="https://github.com/S4vyss" target="_blank">
+                            <img src="../assets/github.svg" width="26" alt="link" />
+                        </a>
+                        <a href="#">
+                            <img src="../assets/twitter.svg" width="26" alt="link" />
+                        </a>
+                        <a href="#">
+                            <img src="../assets/linked.svg" width="24" alt="link" />
+                        </a>
+                        <a href="#">
+                            <img src="../assets/fb.svg" width="16" alt="link" />
+                        </a>
+                    </div>
+                </div>
+                <div class="small-circle">
+
+                </div>
+                <div class="big-circle">
+
+                </div>
+            </div>
+        </article>
+    </main>
+</body>
+</html>
